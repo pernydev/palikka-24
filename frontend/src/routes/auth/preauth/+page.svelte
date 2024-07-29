165c47e1c759c890efa1267106a3e8c46a560f57
Perny <pernydev@gmail.com>
2024-07-29 21:53:03 +0300

    alpu for ya

---
+++ frontend/src/routes/auth/preauth/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { page } from "$app/stores";
+	import { onMount } from "svelte";
+
+    onMount(() => {
+        const token = new URL($page.url).searchParams.get('token');
+        const staff = new URL($page.url).searchParams.get('staff');
+        localStorage.setItem('token', token || "");
+        localStorage.setItem('staff', staff || "");
+
+        goto('/');
+    });
+</script>
